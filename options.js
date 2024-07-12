62890b04305fb273b7fdabb69d16ed11167f2dbd
Russell Billingsley <rbillingsley@users.noreply.github.com>
2024-07-12 18:35:07 +0100

    Prettier formatting pass

--- options.js
+++ options.js
@@ -1,39 +1,33 @@
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
 
-function restoreOptions () {
-    chrome.storage.sync.get(
-        { urls: [] },
-        initialiseConfig()
-    );
+function restoreOptions() {
+  chrome.storage.sync.get({ urls: [] }, initialiseConfig());
 }
 
 function initialiseConfig() {
-    return (config) => {
-        console.log(config);
-        document.getElementById('url').value = config.urls[0];
-    };
+  return (config) => {
+    console.log(config);
+    document.getElementById("url").value = config.urls[0];
+  };
 }
 
-function saveOptions () {
-    const url = document.getElementById('url').value;
-    const urlOptions = [];
-    urlOptions.push(url)
+function saveOptions() {
+  const url = document.getElementById("url").value;
+  const urlOptions = [];
+  urlOptions.push(url);
 
-    console.log(urlOptions);
+  console.log(urlOptions);
 
-    chrome.storage.sync.set(
-      { urls: urlOptions },
-      updateSyncStatus()
-    );
+  chrome.storage.sync.set({ urls: urlOptions }, updateSyncStatus());
 }
 
 function updateSyncStatus() {
-    return () => {
-        const status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(() => {
-            status.textContent = '';
-        }, 750);
-    };
+  return () => {
+    const status = document.getElementById("status");
+    status.textContent = "Options saved.";
+    setTimeout(() => {
+      status.textContent = "";
+    }, 750);
+  };
 }
--- service-worker.js
+++ service-worker.js
@@ -1,64 +1,59 @@
 const storageCache = { count: 0 };
 
 chrome.tabs.onCreated.addListener(blockDuplicateTabs);
-chrome.storage.onChanged.addListener(listenForUserConfigChanges)
+chrome.storage.onChanged.addListener(listenForUserConfigChanges);
 
 async function initStorageCache() {
-    const config = await chrome.storage.sync.get();
-    Object.assign(storageCache, config);
+  const config = await chrome.storage.sync.get();
+  Object.assign(storageCache, config);
 }
 
 async function blockDuplicateTabs(createdTab) {
+  try {
+    await initStorageCache();
+  } catch (error) {
+    console.log(error);
+  }
 
-    try {
-        await initStorageCache();
-    } catch (error) {
-        console.log(error);
-    }
+  const createdTabUrl = createdTab.pendingUrl;
+  const createdTabId = createdTab.id;
+  const currentTabsQueryOption = { active: false, lastFocusedWindow: true };
+  const currentTabs = await chrome.tabs.query(currentTabsQueryOption);
 
-    const createdTabUrl = createdTab.pendingUrl;
-    const createdTabId = createdTab.id; 
-    const currentTabsQueryOption = { active: false, lastFocusedWindow: true};
-    const currentTabs = await chrome.tabs.query(currentTabsQueryOption);
+  console.log("Tab created:", createdTab);
+  console.log("Tabs:", currentTabs);
 
-    console.log("Tab created:", createdTab)
-    console.log("Tabs:", currentTabs)
+  const existingTabIndices = currentTabs
+    .filter(findDuplicateTabs(createdTabUrl))
+    .map((tab) => tab.index)
+    .reverse();
 
-    const existingTabIndices = currentTabs.filter(
-        findDuplicateTabs(createdTabUrl))
-        .map((tab) =>  tab.index)
-        .reverse();
+  console.log("Existing Tabs:", existingTabIndices);
 
-    console.log("Existing Tabs:", existingTabIndices)
+  if (existingTabIndices.length > 0) {
+    // close the new tab as we already have one open
+    await chrome.tabs.remove(createdTabId);
+    console.log("Removed Tab:", createdTabId, createdTab);
 
-    if (existingTabIndices.length > 0)
-    {
-        // close the new tab as we already have one open
-        await chrome.tabs.remove(createdTabId);
-        console.log("Removed Tab:", createdTabId, createdTab)
+    const tabsToHighlight = {
+      tabs: existingTabIndices,
+    };
 
-        const tabsToHighlight= {
-            tabs: existingTabIndices
-        };
+    console.log("Tabs to highlight:", tabsToHighlight);
 
-        console.log("Tabs to highlight:", tabsToHighlight);
-
-        // highlight and focus the existing tab(s)
-        await chrome.tabs.highlight(tabsToHighlight);
-    }
+    // highlight and focus the existing tab(s)
+    await chrome.tabs.highlight(tabsToHighlight);
+  }
 }
 
 function findDuplicateTabs(createdTabUrl) {
-    return (tab) => {
-        let currentTabUrl = tab.url;
-        console.log(createdTabUrl, "===", currentTabUrl);
-        return createdTabUrl === tab.url;
-    };
+  return (tab) => {
+    let currentTabUrl = tab.url;
+    console.log(createdTabUrl, "===", currentTabUrl);
+    return createdTabUrl === tab.url;
+  };
 }
 
-
 async function listenForUserConfigChanges(configChanges, namespace) {
-
-    console.log("User config changed:", configChanges, namespace)
-
+  console.log("User config changed:", configChanges, namespace);
 }
